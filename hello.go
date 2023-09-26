5f65c2f00ab08e8e5339c60fc491d570eeca5f47
Harry Greentree <harrygreentree@icloud.com>
2023-09-26 22:40:07 +0100

    Question 5 - Concurrent Hello World

--- hello.go
+++ hello.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func hello(i int) {
+	fmt.Printf("Hello from goroutine %d\n", i)
+}
 
 func main() {
-	for i := 0; i < 20; i++ {
-		fmt.Println("Hello, World!")
+	for i := 0; i < 5; i++ {
+		go hello(i)
 	}
+	time.Sleep(1 * time.Second)
 }
